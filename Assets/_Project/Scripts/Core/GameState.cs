cb3dc32bb4aa9baf1c20cd28328689017821e454
p4ttt0 <patricio.perezcardenal@gmail.com>
2026-01-14 09:14:24 -0300

    Feat: GameState is created thinking about the possible states along the game

---
+++ Assets/_Project/Scripts/Core/GameState.cs
@@ -0,0 +1,13 @@
+namespace CatRunner.Core
+{
+    public enum GameState
+    {
+        Idle,           // Ovillo colgando - menu
+        CutYarn,        // Corte del ovillo
+        FastTravel,     // Viaje rápido fake
+        SlowDown,       // Frenada del viaje
+        ArrivalIntro,   // Gato baja de la caja
+        Playing,        // Endless runner activo
+        GameOver        // Game over, trasncisión al ovillo colgando
+    }
+}
