90c6bb03d7ebf28e3ff2dd531b2e8446c1d8938e
p4ttt0 <patricio.perezcardenal@gmail.com>
2026-01-14 09:13:15 -0300

    Feat: "CameraController" and "PlayerController" scripts are created empty

---
+++ Assets/_Project/Scripts/Camera/CameraController.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+namespace CatRunner.Camera
+{
+    //TODO: Manejo de la camara accionados a la escucha del GameManager. Transiciones de diferentes zonas.
+    public class CameraController : MonoBehaviour
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/Player/PlayerController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+namespace CatRunner.Player
+{
+    //TODO: Manejo del jugador. Escucha el estado de GameManager y actúa en consecuencia.
+    //TODO: Movimiento, saltos, colisiones, animaciones, etc.
+    public class PlayerController : MonoBehaviour
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
